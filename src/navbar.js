c6bf7f752cb712b713ba7334f4cd76b9e0b372a5
MOHAMMED SAMEER <84911537+sameer9390@users.noreply.github.com>
2023-01-19 14:43:08 +0530

    Update navbar.js

--- src/navbar.js
+++ src/navbar.js
@@ -24,12 +24,7 @@ function Navbar() {
 
       <div id="header__bar" style = {{transform: showHeaderBar? `translateY(120px)`: `translateY(0px)`}}>
         <h3 className="header__bar__title">Frontend Developer</h3>
-        <div className="header__bar__icons">
-          <a href="https://www.facebook.com/dasharath.dasharath.1694/" target='_blank' rel="noreferrer"><img src="https://img.icons8.com/ios/50/000000/facebook--v1.png" alt='icon'/></a>
-          <a href="https://www.instagram.com/dasharath_116/" target='_blank' rel="noreferrer"><img src="https://img.icons8.com/ios/50/000000/instagram-new--v1.png" alt='icon'/></a>
-          <a href="https://github.com/Dasharath9920" target='_blank' rel="noreferrer"><img src="https://img.icons8.com/ios/50/000000/github--v1.png" alt='icon'/></a>
-          <a href="https://www.linkedin.com/in/dyavari-dasharath-b767021b3/" target='_blank' rel="noreferrer"><img src="https://img.icons8.com/ios/50/000000/linkedin.png" alt='icon'/></a>
-        </div>
+       
       </div>
     </div>
   )
